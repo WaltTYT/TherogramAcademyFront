918f242d7a3446eb1c23be093a47f83284aedb74
WaltTYT <2213078585@qq.com>
2026-03-27 17:46:34 +0800

    main

--- src/api/user.js
+++ src/api/user.js
@@ -29,3 +29,37 @@ export const updateUser = (data) => {
 export const deleteUser = () => {
   return request.post('/user/delete')
 }
+
+// 获取用户分页
+export const getUserPage = (data) => {
+  return request.post('/user/page', data)
+}
+
+// 关键字搜索用户分页
+export const searchUserPage = (data) => {
+  return request.post('/user/searchPage', data)
+}
+
+// 上传用户头像
+export const uploadUserAvatar = (id, file) => {
+  const formData = new FormData()
+  formData.append('id', id)
+  formData.append('file', file)
+  return request.post('/user/uploadUser', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+}
+
+// 下载用户头像
+export const downloadUserAvatar = (relativePath) => {
+  return request.get(`/user/downloadUser/${relativePath}`, {
+    responseType: 'blob'
+  })
+}
+
+// 通过ID获取用户名
+export const getUsernameById = (id) => {
+  return request.get(`/user/username/${id}`)
+}
